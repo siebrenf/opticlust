2b3f8af1d7a28e6aa58cb3c0ae5b3cae12c091cd
siebrenf <siebrenf@gmail.com>
2024-06-28 17:01:15 +0200

    WIP renaming

--- pyclustree/tree.py
+++ pyclustree/tree.py
@@ -3,6 +3,7 @@ import warnings
 import matplotlib.pyplot as plt
 import networkx as nx
 import scanpy as sc
+from natsort import natsorted
 
 
 def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=None):
@@ -23,10 +24,13 @@ def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=Non
     :param colors: optional list of colors (default: scanpy defaults)
     :return: plot_data: a dictionary used in clustree_plot
     """
+    columns = natsorted(columns)
     n_clusters = 0
     n_resolutions = len(columns)
     column = None  # column with the highest number of clusters
     for c in columns:
+        if c not in adata.obs:
+            raise ValueError(f"columns not found in adata.obs: '{c}'")
         n = len(adata.obs[c].unique())
         if n > n_clusters:
             n_clusters = n
@@ -50,13 +54,13 @@ def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=Non
             "Column names must be in the shape '[method]_res_[res]' (with [res] a float, e.g. 0.53)"
         )
 
+    # set cluster colors
     if cluster2color is None:
-        # set cluster colors
         cluster2color = {}
         if colors is None:
             colors = sc.pl.palettes.vega_20_scanpy  # default scanpy colors
         i = 0
-        for c in adata.obs[column].unique().sort_values():
+        for c in natsorted(adata.obs[column].unique()):
             # repeat colors if there are more clusters than colors
             if i >= len(colors):
                 i -= len(colors)
@@ -67,17 +71,24 @@ def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=Non
             "You provided both cluster2color and colors. "
             "Argument colors will be ignored."
         )
+    # (over)write the cluster colors in adata.obs, so they will match the tree plot colors
+    for column in columns:
+        adata.uns[f"{column}_colors"] = [
+            color
+            for cluster, color in cluster2color.items()
+            if cluster in adata.obs[column].unique()
+        ]
 
     cluster2barcodes = {}
     r_prev = None
     y = n_resolutions - 1  # plot coordinate
-    for column in sorted(columns):
+    for column in columns:
         r = column.rsplit("_", 1)[1]  # r = resolution, e.g. "0.30"
         assert column == f"{method}_res_{r}"
         y_ticks.append(y)
         y_labels.append(r)
         cluster2barcodes[r] = {}
-        for c in adata.obs[column].unique().sort_values():  # c = cluster name, e.g. "3"
+        for c in natsorted(adata.obs[column].unique()):  # c = cluster name, e.g. "3"
             barcodes = set(adata.obs[adata.obs[column] == c].index)
             cluster2barcodes[r][c] = barcodes
 
@@ -98,8 +109,7 @@ def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=Non
                 dx = (size_node / n_cells) * n_clusters
                 x_node = x + dx / 2
                 y_node = y
-                color_node = cluster2color[c]
-                g.add_node(name, x=x_node, y=y_node, size=size_node, color=color_node)
+                g.add_node(name, x=x_node, y=y_node, size=size_node, label=c)
 
                 x = x + dx
 
@@ -149,10 +159,7 @@ def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=Non
                     dx = (size_node / n_cells) * n_clusters
                     x_node = x + dx / 2
                     y_node = y
-                    color_node = cluster2color[c]
-                    g.add_node(
-                        name, x=x_node, y=y_node, size=size_node, color=color_node
-                    )
+                    g.add_node(name, x=x_node, y=y_node, size=size_node, label=c)
 
                     x = x + dx
 
@@ -162,7 +169,7 @@ def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=Non
         r_prev = r
 
     if rename_cluster:
-        _clustering_rename(adata, g, cluster2barcodes, cluster2color, method)
+        _clustering_rename(adata, g, cluster2barcodes, method)
 
     # plotting needs a bunch of variables, so let's wrap them up nicely
     plot_data = {
@@ -176,92 +183,94 @@ def clustree(adata, columns, rename_cluster=True, cluster2color=None, colors=Non
             "y_labels": y_labels,
         },
         "cluster_method": method,
+        "cluster_colors": cluster2color,
     }
     return plot_data
 
 
-def _clustering_rename(adata, g, cluster2barcodes, cluster2color, method):
-    """rename the clusters adata.obs, such that the clusters names remain consistent over multiple resolutions"""
+def _clustering_rename(adata, g, cluster2barcodes, method):
+    """
+    Rename the clusters names, such that the clusters names remain
+    consistent between resolutions.
+
+    Updates cluster IDs in adata.obs and g.nodes(labels).
+    """
     rename_dict = {}
-    r_prev = None
-    for r in cluster2barcodes.keys():
-        rename_dict[r] = {}
-        if r_prev is None:
-            for c in cluster2barcodes[r].keys():
-                rename_dict[r][c] = f"c{c}"  # no changes
-        else:
-            # sort clusters by overlap with previous clusters (descending)
-            overlaps = {}
-            tmp = dict(
-                sorted(
-                    cluster2barcodes[r_prev].items(),
-                    key=lambda item: len(item[1]),
-                    reverse=True,
-                )
-            )
-            for c_prev, barcodes_prev in tmp.items():
-                overlaps[c_prev] = {}
-                for c, barcodes in cluster2barcodes[r].items():
-                    overlap = len(barcodes & barcodes_prev)
-                    overlaps[c_prev][c] = overlap
-                overlaps[c_prev] = dict(
-                    sorted(
-                        overlaps[c_prev].items(), key=lambda item: item[1], reverse=True
-                    )
-                )
 
-            seen = set()
-            seen2 = set()
-            while len(rename_dict[r]) != len(cluster2barcodes[r]):
-                for c_prev in overlaps.keys():
-                    for c in list(overlaps[c_prev]):
-                        if c in seen:
-                            continue
-                        seen.add(c)
-                        if c_prev in seen2:
-                            c_prev = max([int(i) for i in seen2]) + 1
-                        seen2.add(c_prev)
-                        if c_prev in rename_dict[r_prev]:
-                            rename_dict[r][c] = rename_dict[r_prev][c_prev]
-                        else:
-                            # add a temporary prefix "c" here
-                            # so the clusters aren't merged by pandas.replace
-                            rename_dict[r][c] = f"c{c_prev}"
-                        break
+    # bottom row: 1, 2, 3, 4, etc.
+    r = list(cluster2barcodes)[-1]
+    rename_dict[r] = {}
+    for n, c in enumerate(cluster2barcodes[r]):
+        rename_dict[r][c] = f"c{n}"
+    r_daughters = r
 
-        r_prev = r
+    # other rows: assign cluster to the largest daughter
+    for r_parents in list(cluster2barcodes)[:-1][::-1]:
+        rename_dict[r_parents] = {}
+        seen = []
+        buds = []
+        for c_parent, barcodes_parent in cluster2barcodes[r_parents].items():
+            best = None, 0
+            for c_daughter, barcodes_daughter in cluster2barcodes[r_daughters].items():
+                if c_daughter in seen:
+                    continue
+
+                overlap = len(barcodes_daughter & barcodes_parent)
+                if overlap > best[1]:
+                    best = c_daughter, overlap
+
+            c_daughter = best[0]
+            if c_daughter is None:
+                buds.append(c_parent)
+                continue
+            rename_dict[r_parents][c_parent] = rename_dict[r_daughters][c_daughter]
+            seen.append(c_daughter)
+
+        # when parent clusters merge into one daughter,
+        # rename the smaller parent with leftover cluster names.
+        if buds:
+            clusters = natsorted(set(rename_dict[r_daughters]) - set(seen))
+            for i, c_parent in enumerate(buds):
+                rename_dict[r_parents][c_parent] = rename_dict[r_daughters][clusters[i]]
+
+        r_daughters = r_parents
+
+    # re-order the names so they ascend from top to bottom
+    rename_dict2 = {}
+    clusters = set()
+    for r in rename_dict:
+        for c in rename_dict[r]:
+            clusters.add(rename_dict[r][c])
+    clusters = natsorted(clusters)
+    c2c = {}
+    for r in cluster2barcodes:
+        rename_dict2[r] = {}
+        for c_old in cluster2barcodes[r]:
+            c_new = rename_dict[r][c_old]
+            if c_new not in c2c:
+                c2c[c_new] = clusters.pop(0)
+            rename_dict2[r][c_old] = c2c[c_new]
+    rename_dict = rename_dict2
 
     # rename the clusters in adata
     for r, d in rename_dict.items():
         column = f"{method}_res_{r}"
+        # adata.obs[column] = (
+        #     adata.obs[column]
+        #         .astype("str")  # convert to string
+        #         .rename(d)  # relabel the clusters with "c" prefix
+        #         .str.removeprefix("c")  # "remove the "c" prefix
+        #         # .astype("category")  # convert back to categorical
+        # )
         adata.obs[column] = adata.obs[column].cat.rename_categories(d)
         # "remove the "c" prefix
         adata.obs[column] = adata.obs[column].str.removeprefix("c")
-        # overwrite the cluster colors in adata.obs, in case a custom color palette was given
-        adata.uns[f"{column}_colors"] = [
-            col for c, col in cluster2color.items() if c in rename_dict[r]
-        ]
 
-    # rename the clusters in g
-    node_mapping_name1 = {}
-    node_mapping_name2 = {}
-    node_mapping_color = {}
-    for r, d in rename_dict.items():
-        for c_old, c_new in d.items():
-            node_old = f"{r}_{c_old}"
-            node_new1 = f"{r}_{c_new}"
-            node_new2 = f"{r}_{c_new[1:]}"  # remove the "c" prefix
-            node_mapping_name1[node_old] = node_new1
-            node_mapping_name2[node_new1] = node_new2
-
-            color_new = cluster2color[c_new[1:]]
-            node_mapping_color[node_old] = color_new
-
-    # update the node colors
-    nx.set_node_attributes(g, name="color", values=node_mapping_color)
-    # update the node labels
-    nx.relabel_nodes(g, node_mapping_name1, copy=False)
-    nx.relabel_nodes(g, node_mapping_name2, copy=False)
+    # leave the node name, but rename the node label
+    for node, md in g.nodes(data=True):
+        r, c = node.split("_")
+        label = rename_dict[r][c]
+        md["label"] = label[1:]  # "remove the "c" prefix
 
 
 def clustree_plot(
@@ -301,6 +310,7 @@ def clustree_plot(
     y_ticks = plot_data["axis"]["y_ticks"]
     y_labels = plot_data["axis"]["y_labels"]
     method = plot_data["cluster_method"]
+    cluster2color = plot_data["cluster_colors"]
     if subplot_kwargs is None:
         subplot_kwargs = {}
     if node_kwargs is None:
@@ -334,17 +344,21 @@ def clustree_plot(
     for n, attrs in g.nodes(data=True):
         pos[n] = (attrs["x"], attrs["y"])
         # scale to [0, 1]
-        s = (attrs["size"] - node_size_min) / (node_size_max - node_size_min)
+        s = 1
+        if node_size_max != node_size_min:
+            s = (attrs["size"] - node_size_min) / (node_size_max - node_size_min)
         # scale to specified node_sizes
         s = (node_sizes[1] - node_sizes[0]) * s + node_sizes[0]
         node_sizes_.append(s)
-        node_colors.append(attrs["color"])
-        node_labels[n] = n.split("_")[1]
+        node_colors.append(cluster2color[attrs["label"]])
+        node_labels[n] = attrs["label"]
 
     edge_widths = []
     for u, v, attrs in g.edges(data=True):
         # scale to [0, 1]
-        s = (attrs["size"] - edge_size_min) / (edge_size_max - edge_size_min)
+        s = 1
+        if edge_size_max != edge_size_min:
+            s = (attrs["size"] - edge_size_min) / (edge_size_max - edge_size_min)
         # scale to specified edge_sizes
         s = (edge_sizes[1] - edge_sizes[0]) * s + edge_sizes[0]
         edge_widths.append(s)
