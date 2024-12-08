3b91a84dcc5c5d4b1f155e1b04e426581ae18633
Arts-of-coding <julian.armando.arts@gmail.com>
2024-12-08 19:34:12 +0100

    revised unit tests

--- tests/test_01.py
+++ tests/test_01.py
@@ -11,6 +11,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pytest
 import scanpy as sc
 
 from opticlust.clust import clustering, clustering_plot
@@ -40,12 +41,11 @@ def test_load_data_positive():
     assert isinstance(adata.X, np.ndarray) == 1
 
 
-def test_load_data_negative():
-    # load data (adata.obs should not be anything else)
-    adata = sc.datasets.pbmc68k_reduced()
-    assert not isinstance(adata.obs, pd.DataFrame) == 0
-    assert not isinstance(adata.var, pd.DataFrame) == 0
-    assert not isinstance(adata.X, np.ndarray) == 0
+def test_load_data_fail():
+    with pytest.raises(Exception) as excinfo:
+        adata = pd.DataFrame()
+        isinstance(adata.obs, pd.DataFrame)
+    assert str(excinfo.value) == "'DataFrame' object has no attribute 'obs'"
 
 
 # Load in data if above tests work
@@ -58,9 +58,10 @@ def test_clustering_positive():
     assert len(columns) == 81
 
 
-def test_clustering_negative():
-    columns = clustering(adata)
-    assert not isinstance(columns, list) == 0
+def test_clustering_fail():
+    with pytest.raises(Exception) as excinfo:
+        columns = clustering(anndata)
+    assert str(excinfo.value) == "name 'anndata' is not defined"
 
 
 # Define the columns if above tests work
@@ -70,23 +71,24 @@ columns = clustering(adata)
 # Run unit tests on clustering plot and check length of list
 def test_clusteringplot_positive():
     tree_columns = clustering_plot(adata, columns)
-    plt.close()
     assert isinstance(tree_columns, list) == 1
     assert len(tree_columns) == 11
 
 
-def test_clusteringplot_negative():
-    tree_columns = clustering_plot(adata, columns)
-    plt.close()
-    assert not isinstance(tree_columns, list) == 0
+def test_clusteringplot_fail():
+    with pytest.raises(Exception) as excinfo:
+        columns = ["random1", "random2"]
+        tree_columns = clustering_plot(adata, columns)
+    assert (
+        str(excinfo.value) == "Column names must be in the shape '[method]_res_[res]'"
+    )
 
 
-# Define tree columns if the above tests work
 tree_columns = clustering_plot(adata, columns)
 
 
 # Run unit tests on graph building and check if graph contains all
-def test_buildtree():
+def test_buildtree_tree_data():
     tree_data = clustree(adata, tree_columns, rename_cluster=True)
     assert isinstance(tree_data, dict) == 1
     assert "graph" in tree_data
@@ -94,4 +96,49 @@ def test_buildtree():
     assert "axis" in tree_data
 
 
-# To do: implement testing the clustree_plot in some way?
+# Run unit tests on graph building and check if graph contains all
+def test_buildtree_tree_data_rename_false():
+    tree_data = clustree(adata, tree_columns, rename_cluster=False)
+    assert isinstance(tree_data, dict) == 1
+    assert "graph" in tree_data
+    assert "dimensions" in tree_data
+    assert "axis" in tree_data
+
+
+# Use column instead of tree_columns if users do not want tree columns
+adata2 = sc.datasets.pbmc68k_reduced()
+columns2 = clustering(adata2)  # , samples=3
+
+
+def test_buildtree_column_data():
+    tree_data2 = clustree(adata2, columns2, rename_cluster=True)
+    assert isinstance(tree_data2, dict) == 1
+    assert "graph" in tree_data2
+    assert "dimensions" in tree_data2
+    assert "axis" in tree_data2
+
+
+def test_buildtree_column_data_rename_false():
+    tree_data2 = clustree(adata2, columns2, rename_cluster=False)
+    assert isinstance(tree_data2, dict) == 1
+    assert "graph" in tree_data2
+    assert "dimensions" in tree_data2
+    assert "axis" in tree_data2
+
+
+def test_buildtree_fail():
+    with pytest.raises(Exception) as excinfo:
+        tree_columns3 = ["random1", "random2"]
+        tree_data3 = clustree(adata, tree_columns3, rename_cluster=True)
+    assert str(excinfo.value) == "columns not found in adata.obs: 'random1'"
+
+
+def test_plottree_tree_data():
+    tree_data = clustree(adata, tree_columns, rename_cluster=False)
+    clustree_plot(tree_data)
+
+
+# Test why this test below fails
+#def test_plottree_tree_data_rename_cluster():
+#    tree_data = clustree(adata, tree_columns, rename_cluster=True)
+#    clustree_plot(tree_data)
